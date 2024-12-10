c1544566139c917adcc0db2ea07ba927d5e4b2ee
Yusef <youssefmohmed430@gmail.com>
2024-12-10 14:07:55 +0200

    comments in filemanager

--- src/main/java/com/mycompany/library_management_system/FileManager.java
+++ src/main/java/com/mycompany/library_management_system/FileManager.java
@@ -3,24 +3,26 @@ package com.mycompany.library_management_system;
 import java.io.*;
 
 public class FileManager {
-    public static void saveObject(Object ob, String filePath) 
+    public static void saveObject(Object ob, String filePath) // دالة لحفظ ال objects في ال files
     {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
-            oos.writeObject(ob);
+            // ObjectOutputStream كلاس بيستخدم لكتابة بيانات ال  objects في الملفات 
+            oos.writeObject(ob);// دالة بتستدعيها من أوبجكت لكتابة البيانات
         }
-        catch (IOException e) {
+        catch (IOException e) { 
             System.err.println("Error saving user: " + e.getMessage());
         }
     }
     
-    public static User LoadUser(String Username , String Password,String filepath)
+    public static User LoadUser(String Username , String Password,String filepath) 
+            // دالة عشان تجيب ال objects من ال files
     {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filepath))) {
               while(true)
               {
                   try 
                   {
-                      User user = (User) ois.readObject();
+                      User user = (User) ois.readObject(); //هنا بتعمل read للobject
                       if(user.UserName.equals(Username) && user.Password.equals(Password))
                       {
                           return user;
@@ -38,7 +40,7 @@ public class FileManager {
         return null;
     }
     
-    public static Object LoadBook( String title,String BookId,String filepath)
+    public static Object LoadBook( String title,String BookId,String filepath) // دالة للحصول علي ال objects الكتب
     {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filepath))) {
               while(true)
